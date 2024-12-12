a6704b0a0f385d8eef67aaabbfc96289c9a56303
Develing1991 <completed0728@gmail.com>
2024-12-12 15:09:54 +0900

    커스텀 디렉티브 replace 추가

--- src/directives/katex-directive.js
+++ src/directives/katex-directive.js
@@ -2,23 +2,44 @@ import katex from 'katex'
 import renderMathInElement from 'katex/dist/contrib/auto-render.mjs'
 import merge from 'deepmerge'
 
+const replaceKatex = (text, type) => {
+    const regex = /(\$\$.*?\$\$|\$.*?\$|\\\(.*?\\\)|\\\[.*?\\\])/gs;
+    // el.innerHTML = el.innerHTML.replace(/\$([^$]+)\$/g, (match, math) => {
+    return text.replace(regex, (match, math) => {
+    try {
+        const isBlock = match.startsWith("$$") || match.startsWith("\\[");
+        const mathContent = match
+            .replace(/^\$\$|^\$|^\(\\|^\[\\|\\\)$|\\\]$|\$$/g, "");
+        if(type === 'directive'){
+            return mathContent
+        }
+        return katex.renderToString(mathContent, { 
+            displayMode: isBlock,
+            throwOnError: false 
+        });
+        
+    } catch (e) {
+        return match; // 오류가 있으면 원본 반환
+    }
+    }); 
+}
 const katexDirective = (globalOptions) => ({
   name: 'katex',
   directive: function (el, binding) {
     const argOptions = (binding.value && binding.value.options) || {}
     const allOptions = merge(globalOptions, argOptions)
-
+    // debugger
     if (binding.arg && binding.arg === 'auto') {
-      renderMathInElement(el, allOptions)
+        el.innerHTML = replaceKatex(el.innerHTML, 'innerHtml')
+        renderMathInElement(el, allOptions)
     } else {
-      const expression = binding.value.expression || binding.value
-      const displayMode = {}
-      if (binding.arg === 'display') {
-        displayMode.displayMode = true
-      }
-      const options = merge(allOptions, displayMode)
-
-      katex.render(expression, el, options)
+        const expression = replaceKatex(binding.value.expression || binding.value, 'directive')
+        const displayMode = {}
+        if (binding.arg === 'display') {
+            displayMode.displayMode = true
+        }
+        const options = merge(allOptions, displayMode)
+        katex.render(expression, el, options)
     }
   },
 })
