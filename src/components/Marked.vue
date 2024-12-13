f8306e6ba1e236024237a42183e64fa3d7cb2e06
Develing1991 <completed0728@gmail.com>
2024-12-13 14:32:59 +0900

    add code header

--- src/components/Marked.vue
+++ src/components/Marked.vue
@@ -2,15 +2,21 @@
     <div v-html="markdownText" style="width: 500px; text-align: left;">
 
     </div>
+    
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, nextTick } from 'vue'
 import {marked} from 'marked'
 import markedKatex from "marked-katex-extension";
 import { markedHighlight } from "marked-highlight";
 import hljs from 'highlight.js';
-import 'highlight.js/styles/github-dark.css';
+import 'highlight.js/styles/gradient-light.css';
+import ClipboardJS from 'clipboard'
+import { v4 as uuidv4 } from 'uuid';
+// import { useClipboard } from '@vueuse/core'
+
+// const { text, copy, copied, isSupported } = useClipboard()
 const markdownText = ref('')
 const getText = ref(`
 
@@ -33,8 +39,8 @@ ____saddsa____
 <br>
 
 \`\`\`javascript
-const highlight = "code";
-function aaa () {
+const highlight = "code1";
+function aaa1 () {
     return 1
 \}
 \`\`\`
@@ -53,10 +59,17 @@ $$
 > sdf
 >> dsfsd
 
-\`\`\`javascript
-const highlight = "code";
-function aaa () {
-    return 1
+\`\`\`java
+public static void final aaa = "hi";
+public String message(String msg){
+    return msg;
+}
+\`\`\`
+
+\`\`\`
+public static void final aaa = "hi";
+function aaa2 () {
+    return 2
 \}
 \`\`\`
 
@@ -82,8 +95,51 @@ onMounted(() => {
     marked.use(markedKatex({ nonStandard: true }));
     markdownText.value = getMarkDown()
 
-    // lang, copy button
-    document.querySelectorAll('pre').forEach(el => console.log(el))
+    nextTick(() => {
+         // lang, copy button
+        const codeBlocks = document.querySelectorAll('pre')
+        // chldren('code') -> 의 class="hljs language-java" 에서 language 추출 후  codeLanguageEl.innerHTML  로 넣어주기 
+        codeBlocks.forEach(block => {
+            // block.children.
+            
+            const codeEl = Array.from(block.children).find(child => child.tagName === 'CODE')
+            
+            const codeLanguage = Array.from(codeEl.classList).find(className => className.startsWith('language'))?.split('-')[1] ?? 'plain text'
+            // debugger;
+            const codeBlockUuid = uuidv4()
+            // const { text, isSupported, copy } = useClipboard()
+            const headerWrapperEl = document.createElement('div')
+            const codeLanguageEl = document.createElement('div')
+            const codeCopyButtonEl = document.createElement('button')
+
+            block.setAttribute('style', 'margin-top: 0px; border-radius: 0 0 5px 5px; overflow: hidden;')
+            block.setAttribute('id', `target-${codeBlockUuid}`)
+            codeLanguageEl.innerHTML = codeLanguage
+            codeCopyButtonEl.innerHTML = 'COPY'
+            codeCopyButtonEl.classList.add(`copy-btn-${codeBlockUuid}`)
+            codeCopyButtonEl.setAttribute('data-clipboard-target', `#target-${codeBlockUuid}`)
+            codeCopyButtonEl.setAttribute('style', 'padding: 3px 8px; border: none; outline: none; display: flex; justify-content: center; align-items: center; border-radius: 2px;')
+            codeCopyButtonEl.addEventListener('click', () => {
+                // window.getSelection().removeAllRanges()
+                codeCopyButtonEl.innerHTML = 'COPIED'
+                codeCopyButtonEl.disabled = true
+                codeCopyButtonEl.style.cursor = 'default'
+                const timer = setTimeout(() => {
+                    // window.getSelection()!.removeAllRanges()
+                    codeCopyButtonEl.innerHTML = 'COPY'
+                    codeCopyButtonEl.disabled = false
+                    codeCopyButtonEl.style.cursor = 'pointer'
+                    clearTimeout(timer);
+                }, 2000);
+            })
+            headerWrapperEl.setAttribute('style', 'color: #ebebeb; background-color: #2f2f2f; height: 40px; display:flex; justify-content:space-between; align-items: center; padding: 5px 10px; border-radius: 5px 5px 0 0;')
+            headerWrapperEl.append(codeLanguageEl)
+            headerWrapperEl.append(codeCopyButtonEl)
+            block.parentNode.insertBefore(headerWrapperEl, block);
+            new ClipboardJS(`.copy-btn-${codeBlockUuid}`);
+        })
+    })
+   
 })
 </script>
 
